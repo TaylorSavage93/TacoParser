b3d217a01929618281745c9dfbfb3ec745c5928d
Taylor Savage <taylorsavage93@gmail.com>
2022-07-26 15:03:42 -0400

    finished tacoparser project

--- LoggingKata/Program.cs
+++ LoggingKata/Program.cs
@@ -20,38 +20,65 @@ namespace LoggingKata
             // use File.ReadAllLines(path) to grab all the lines from your csv file
             // Log and error if you get 0 lines and a warning if you get 1 line
             var lines = File.ReadAllLines(csvPath);
+            if(lines.Length == 0)
+            {
+                logger.LogError("file has no data");
+            }
+            if(lines.Length == 1)
+            {
+                logger.LogWarning("only one line of input in this file");
+            }
 
-            logger.LogInfo($"Lines: {lines[0]}");
+            //logger.LogInfo($"Lines: {lines[0]}");
 
             // Create a new instance of your TacoParser class
             var parser = new TacoParser();
 
             // Grab an IEnumerable of locations using the Select command: var locations = lines.Select(parser.Parse);
-            var locations = lines.Select(parser.Parse).ToArray();
+            ITrackable[] locations = lines.Select(x => parser.Parse(x)).ToArray();
 
             // DON'T FORGET TO LOG YOUR STEPS
 
             // Now that your Parse method is completed, START BELOW ----------
 
-            // TODO: Create two `ITrackable` variables with initial values of `null`. These will be used to store your two taco bells that are the farthest from each other.
-            // Create a `double` variable to store the distance
-
-            // Include the Geolocation toolbox, so you can compare locations: `using GeoCoordinatePortable;`
+            // Done TODO: Create two `ITrackable` variables with initial values of `null`. These will be used to store your two taco bells that are the farthest from each other.
+            // Done Create a `double` variable to store the distance
+            ITrackable tacoBell1 = null;
+            ITrackable tacoBell2 = null;
+            double distance = 0;
+            //Done Include the Geolocation toolbox, so you can compare locations: `using GeoCoordinatePortable;`
 
             //HINT NESTED LOOPS SECTION---------------------
             // Do a loop for your locations to grab each location as the origin (perhaps: `locA`)
+            for (int i = 0; i < locations.Length; i++)
+            {
+                var locA = locations[i];
+                GeoCoordinate cordA = new GeoCoordinate(locA.Location.Latitude, locA.Location.Longitude);
 
-            // Create a new corA Coordinate with your locA's lat and long
+                for(int j = 0; j < locations.Length; i++)
+                {
+                    var locB = locations[j];
+                    GeoCoordinate cordB = new GeoCoordinate(locB.Location.Latitude, locB.Location.Longitude);
 
-            // Now, do another loop on the locations with the scope of your first loop, so you can grab the "destination" location (perhaps: `locB`)
+                    if(cordA.GetDistanceTo(cordB) > distance)
+                    {
+                        distance = Math.Round(cordA.GetDistanceTo(cordB), 2);
+                        tacoBell1 = locA;
+                        tacoBell2 = locB;
+                    }
+                }
+            }
+            //Done Create a new corA Coordinate with your locA's lat and long
 
-            // Create a new Coordinate with your locB's lat and long
+            // Done Now, do another loop on the locations with the scope of your first loop, so you can grab the "destination" location (perhaps: `locB`)
 
-            // Now, compare the two using `.GetDistanceTo()`, which returns a double
+            // Done Create a new Coordinate with your locB's lat and long
+
+            // Done Now, compare the two using `.GetDistanceTo()`, which returns a double
             // If the distance is greater than the currently saved distance, update the distance and the two `ITrackable` variables you set above
 
             // Once you've looped through everything, you've found the two Taco Bells farthest away from each other.
-
+            Console.WriteLine($"The distance between {tacoBell1.Name} and {tacoBell2.Name} is {distance} miles");
 
             
         }
--- LoggingKata/TacoParser.cs
+++ LoggingKata/TacoParser.cs
@@ -9,7 +9,7 @@
         
         public ITrackable Parse(string line)
         {
-            logger.LogInfo("Begin parsing");
+            //logger.LogInfo("Begin parsing");
 
             // Take your line and use line.Split(',') to split it up into an array of strings, separated by the char ','
             var cells = line.Split(',');
@@ -17,28 +17,39 @@
             // If your array.Length is less than 3, something went wrong
             if (cells.Length < 3)
             {
-                // Log that and return null
+                // Done Log that and return null
+                logger.LogWarning("array is less than 3 items. data is incomplete");
                 // Do not fail if one record parsing fails, return null
                 return null; // TODO Implement
             }
 
-            // grab the latitude from your array at index 0
-            // grab the longitude from your array at index 1
-            // grab the name from your array at index 2
+            // Done grab the latitude from your array at index 0
+            var latitude = double.Parse(cells[0]);
+            // Done grab the longitude from your array at index 1
+            var longitude = double.Parse(cells[1]);
+            // Done grab the name from your array at index 2
+            var name = cells[2];
+            // Done Your going to need to parse your string as a `double`
+            // Done which is similar to parsing a string as an `int`
 
-            // Your going to need to parse your string as a `double`
-            // which is similar to parsing a string as an `int`
+            // Done You'll need to create a TacoBell class
+            // Done that conforms to ITrackable
 
-            // You'll need to create a TacoBell class
-            // that conforms to ITrackable
+            // Done Then, you'll need an instance of the TacoBell class
+            // Done With the name and point set correctly
+            var point = new Point();
+            point.Latitude = latitude;
+            point.Longitude = longitude;
+            
+            var tacoBell = new TacoBell();
+            tacoBell.Name = name;
+            tacoBell.Location = point;
 
-            // Then, you'll need an instance of the TacoBell class
-            // With the name and point set correctly
-
-            // Then, return the instance of your TacoBell class
+            // Done Then, return the instance of your TacoBell class
+            return tacoBell;    
             // Since it conforms to ITrackable
 
-            return null;
+           
         }
     }
 }
